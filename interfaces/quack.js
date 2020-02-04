17f0ba86a82eaf1daad00a815d87648430a845e9
johao70 <perlazajohao@gmail.com>
2020-02-04 17:40:00 -0500

    interfaces, models

---
+++ interfaces/quack.js
@@ -0,0 +1,11 @@
+class QuackBehavior {
+    quack() {
+        console.log('realizar quack');
+    }
+
+    noQuack() {
+        console.log('silencio');
+    }
+}
+
+module.exports = QuackBehavior;
